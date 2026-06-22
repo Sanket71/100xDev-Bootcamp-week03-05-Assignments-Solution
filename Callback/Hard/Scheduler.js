41947b7b97e0356598bd96d1d797f47bd34420b3
Sanket <88245320+Sanket71@users.noreply.github.com>
2026-06-22 12:56:26 +0530

    added solution of easy problems of Promises of week 3-5

--- Callback/Hard/Scheduler.js
+++ Callback/Hard/Scheduler.js
@@ -10,12 +10,55 @@
 // True preemption is not possible in JavaScript, so tasks must cooperate
 // by yielding execution voluntarily.
 
+class PriorityQueue{
+  constructor(){
+    this.queue=[];
+  }
+
+  enqueue(task,priority){
+    this.queue.push({task,priority});
+    this.queue.sort((a,b)=>b.priority-a.priority);
+  }
+  
+  deque(){
+    return this.queue.shift();
+  }
+
+  peek(){
+    return this.queue[0];
+  }
+
+  isEmpty(){
+    return this.queue.length===0;
+  }
+}
+
 class Scheduler {
-  constructor() {}
+  constructor() {
+    this.pq=new PriorityQueue();
+  }
 
-  schedule(task, priority = 0) {}
+  schedule(task, priority = 0) {
+    this.pq.enqueue(task,priority);
+  }
 
-  run(onAllFinished) {}
+  run(onAllFinished) {
+    const runNext=()=>{
+      if(this.pq.isEmpty()){
+        return onAllFinished(null);
+      }
+
+      const {task}=this.pq.deque();
+
+      task((err,result)=>{
+        if(err){
+          return onAllFinished(err);
+        }
+        runNext();
+      })
+    }
+    runNext();
+  }
 }
 
 module.exports = Scheduler;
---
+++ Promises/Easy/promiseAll.js
@@ -0,0 +1,14 @@
+// Problem Description – Custom Implementation of Promise.all
+
+// You are required to implement your own version of Promise.all without using the built-in method. 
+// The function should accept an array of values that may include Promises or plain constants. 
+// It must resolve with an array of results in the same order once all inputs resolve, or reject immediately if any input rejects.
+function promiseAll(promises) {
+    const result=[];
+    for(const promise of promises){
+        result.push(await promise);
+    }
+    return result;
+}
+
+module.exports = promiseAll;
